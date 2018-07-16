0b13dc761da7781b0aeabe6a15073411a3a62c8e
StupidDan <danielbee912@gmail.com>
2018-07-16 18:59:03 +0100

    Added ESLint, and added eval command and added it to the help menu

---
+++ commands/eval.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js");
+const settings = require("../settings.json");
+
+module.exports = async(client, msg, suffix) => {
+	/*
+		Eval Command by SunburntRock89 (https://github.com/SunburntRock89)
+	*/
+	try {
+		const embed1 = new Discord.RichEmbed();
+		const embed2 = new Discord.RichEmbed();
+		const embed3 = new Discord.RichEmbed();
+		if (suffix.startsWith("```js") && suffix.endsWith("```")) suffix = suffix.substring(5, suffix.length - 3);
+		const asyncify = code => `(async () => {\nreturn ${code.trim()}\n})()`;
+		let result = await eval(asyncify(suffix));
+		if (typeof result !== "string") result = require("util").inspect(result, false, 1);
+		const array = [
+			client.token.escapeRegex(),
+			settings.token.escapeRegex(),
+		];
+		const regex = new RegExp(array.join("|"), "g");
+		result = result.replace(regex, "Nice try.");
+		if (suffix.length == 0) {
+			embed1.setDescription("You need to input something to evaluate!")
+				.setColor("#FF6347");
+			return msg.channel.send({ embed: embed1 });
+		}
+		if (msg.author.id !== "357583052525928449") {
+			embed2.setDescription("You don't have permissions to run this command! ⚠️")
+				.setColor("#FF6347");
+			return msg.channel.send({ embed: embed2 });
+		}
+		embed3.setDescription(`${"Code executed successfully. ✅ ```js\n"}${result}\`\`\``)
+			.setColor("#00FA9A");
+		msg.channel.send({ embed: embed3 });
+	} catch (err) {
+		const embed4 = new Discord.RichEmbed();
+		embed4.setDescription(`${"There seemed to have been an error executing that code, take a look: ⚠️ \n ```js\n"}${err}\`\`\``)
+			.setColor("#FF6347");
+		return msg.channel.send({ embed: embed4 });
+	}
+};
--- commands/help.js
+++ commands/help.js
@@ -1,18 +1,19 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 module.exports = async(client, msg, suffix) => {
 	const embed = new Discord.RichEmbed();
 	const DMs = new Discord.RichEmbed();
 	embed.setTitle("BloxBot Help Menu")
-	.setDescription("Here is a list of commands that BloxBot has.")
-	.addField("ping", "Tests the ping connection of the bot with a milliseconds value.", true)
-	.addField("help", "DMs you this list of commands.", true)
-	.addField("getrole", "Gets the role of a particular user in any Roblox group.", true)
-	.setColor("#00BFFF")
-	.setThumbnail(client.user.avatarURL)
-	msg.author.send({embed});
+		.setDescription("Here is a list of commands that BloxBot has.")
+		.addField("ping", "Tests the ping connection of the bot with a milliseconds value.", true)
+		.addField("help", "DMs you this list of commands.", true)
+		.addField("getrole", "Gets the role of a particular user in any Roblox group.", true)
+		.addField("eval", "Allows you to evaluate JavaScript code. **Developers Only**", true)
+		.setColor("#00BFFF")
+		.setThumbnail(client.user.avatarURL);
+	msg.author.send({ embed });
 
 	DMs.setDescription("Check your DMs. 📩")
-	.setColor("#00BFFF")
-	msg.channel.send({embed: DMs});
-}
+		.setColor("#00BFFF");
+	msg.channel.send({ embed: DMs });
+};
